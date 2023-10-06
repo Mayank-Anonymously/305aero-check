cd2bccbf871bffaa193552bab3b75fb62bbb91af
MayankTravel <mayank.kumar@midastravel.org>
2023-10-06 14:12:23 +0530

    New Changesss

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Header from 'components/header';
 import { useRouter } from 'next/router';
+import Header from '../components/header';
 
 type LayoutType = {
   title?: string;
